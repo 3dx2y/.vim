2ac6142c23bdd022ab7fecf188f09560c278db20
Guangyu Suo <guangyu@unitedstack.com>
2014-02-10 19:48:26 +0800

    add git config

--- install.sh
+++ install.sh
@@ -20,9 +20,14 @@ sudo apt-get install -y pyflakes exuberant-ctags
 pip install flake8
 else
 yum --enablerepo install -y pyflakes
-yum install -y ctags-etags
+# yum install -y ctags-etags
 fi
 
 git config --global user.name "Guangyu Suo"
 git config --global user.email "guangyu@unitedstack.com"
 git config --global core.excludesfile $HOME/.gitignore
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
